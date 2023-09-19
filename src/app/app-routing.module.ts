4a2bd7e7188851d5f7557627c757c816ead12221
OCTAVIO HERRERA <octavio.herrera@bam.com.gt>
2023-09-18 23:12:21 -0600

    logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListmanagerComponent } from './components/listmanager/listmanager.component';
+import { ResetpaswordComponent } from './components/resetpasword/resetpasword.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listmanager', component: ListmanagerComponent },
+  { path: 'resetpasword', component: ResetpaswordComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // Puedes agregar más rutas aquí
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ResetpaswordComponent } from './components/resetpasword/resetpasword.component';
+import { ListmanagerComponent } from './components/listmanager/listmanager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ResetpaswordComponent,
+    ListmanagerComponent
   ],
   imports: [
     BrowserModule,
